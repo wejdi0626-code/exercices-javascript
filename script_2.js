05213ae395c5de9b5fb40b162803c047dad76557
wejdi0626-code <wejdi.0626@gmail.com>
2026-07-06 15:51:16 +0200

    Delete script_2.js

--- script_2.js
+++
@@ -1,8 +0,0 @@
-let nombre = Number(prompt("De quel nombre veux-tu calculer la factorielle ?"));
-let resultat = 1;
-
-for (let i = nombre; i >= 1; i--) {
-  resultat = resultat * i;
-}
-
-console.log("Le résultat est : " + resultat);
